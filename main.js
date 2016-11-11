7fcb08593f73efffc522c1efcd2ea7a8ba566f72
yz1184@nyu.edu <yz1184@nyu.edu>
2016-11-10 22:30:56 -0500

    cleaned data and updated output

--- main.js
+++ main.js
@@ -1,47 +1,64 @@
+
+//   =========================
+//   ---- GLORIOUS NOTES -----
+//   =========================
+ // 1. BEWARE DATA TYPE&FORMATS
+ //For Hernando, the data type from the .json file is string and it's formatted with commas
+ //Even parseInt() would not solve the problem. While it can convert string to number, it only returns what's before the comma. 
+ //E.g. The datum is "1,023", then parseInt() would return 1 instead of 1023. 
  
+
  
 
 var csvWriter=require("csv-write-stream");
 var writer=csvWriter();
 var fs=require("fs");
 var turf=require("@turf/turf");
-var precinct=require("./reader"); 
-var votes=require("./jsonReader");
+var precinct=require("./geoJSON/precinct_Hernandos"); 
+var votes=require("./source_data/JSON_Hernandos");
 
-
-writer.pipe(fs.createWriteStream("test.csv")); 
-
-
-
-//L.mapbox.accessToken = 'pk.eyJ1IjoiaGVsbHVpbiIsImEiOiJjaXY5Mjh3ejkwMGRyMnRwYzJkNXl4OGkxIn0.7Ram_00U6U26M5NKFnqvxw';
-
+writer.pipe(fs.createWriteStream("Dots_Hernando_DEM.csv")); 
 
 numOfPrecinct= precinct.features.length;
-console.log(precinct.features.length);
+
+
 
 for(var i=0; i<numOfPrecinct; i ++) { 
 
     var numOfVotes_DEM;
     var numOfVotes_REP; 
 
+
    //GETTING BOUNDING BOX OF CURRENT PRECINCT
    var boundingBox=turf.bbox(precinct.features[i]);
 
    //GETTING CURRENT PRECINCT CODE
-   var currentPrecinct=precinct.features[i].properties.PRECINCT;
+   var currentPrecinct=precinct.features[i].properties.PRECNCTNO;
+   console.log("Inspecting votes in precinct: " + currentPrecinct); 
+
+   console.log("This is the "+i+"th precinct");
 
    //LOOK FOR PRECINCT BASED ON CODE IN THE VOTES DATASET
    for (var k=0; k<votes.length; k ++ ) { 
-    console.log (votes[k].Party.toUpperCase());
+      
      if (votes[k].Precinct  == currentPrecinct  && 
         votes[k].Party.toUpperCase() == "DEM") { 
-         numOfVotes_DEM=votes[k].Votes; 
-         createDots_DEM(numOfVotes_DEM ); 
 
-     } else if(votes[k].Precinct  == currentPrecinct  && 
+         numOfVotes_DEM=votes[k].Votes; 
+        //console.log("about to draw for DEM")
+         createDots(numOfVotes_DEM ); 
+
+
+     } 
+     else if(votes[k].Precinct  == currentPrecinct  && 
         votes[k].Party.toUpperCase() == "REP"){ 
+        
          numOfVotes_REP=votes[k].Votes; 
-         createDots_REP(numOfVotes_REP ); 
+
+         //createDots(numOfVotes_REP ); 
+         //console.log( numOfVotes_REP);
+         //console.log( parseInt(numOfVotes_REP));
+
      }  
 
    }
@@ -49,15 +66,18 @@ for(var i=0; i<numOfPrecinct; i ++) {
 }
 
  
-function createDots_DEM(num) { 
-  console.log("a vote for DEM!");
+function createDots(num) { 
+  //console.log("a vote for DEM!");
  var poly=turf.bboxPolygon(boundingBox) ;
  var x_min=boundingBox[0];
  var x_max=boundingBox[2];
  var y_min=boundingBox[1];
  var y_max=boundingBox[3]; 
+ console.log("There are " + num +" points to be drawn in this precinct"); 
+ var j=0;
 
- var j=0; 
+ // console.log( parseInt(num));
+ // console.log(j<parseInt(num));
  while(j<num){ 
      var lat = y_min + (Math.random() * (y_max - y_min));
      var lng = x_min + (Math.random() * (x_max - x_min));
@@ -66,36 +86,13 @@ function createDots_DEM(num) {
      var inside = turf.inside(point, precinct.features[i]);
        
        if (inside) {
-           //writer.write({ "x": lng, "y":lat })
-         j+=1;           
+          writer.write({ "x": lng, "y":lat })
+         console.log("Now drawing the "+ j + "th point");
+         j+=1;              
+               
       }  
  }
 }
-
-
-function createDots_REP(num) { 
-  console.log("a vote for REP");
- var poly=turf.bboxPolygon(boundingBox) ;
- var x_min=boundingBox[0];
- var x_max=boundingBox[2];
- var y_min=boundingBox[1];
- var y_max=boundingBox[3]; 
-
- var j=0; 
- while(j<num){ 
-     var lat = y_min + (Math.random() * (y_max - y_min));
-     var lng = x_min + (Math.random() * (x_max - x_min));
-
-     var point  = turf.point([lng, lat]);
-     var inside = turf.inside(point, precinct.features[i]);
-       
-       if (inside) {
-            writer.write({ "x": lng, "y":lat })
-         j+=1;           
-      }  
- }
-}
-
 
  
 
